de5a7fdc39577b6830b21a68028cbef56bdd7841
Eirik Vageskar <eirikvageskar@gmail.com>
2019-05-18 14:57:52 +0200

    Updates

---
+++ precache-manifest.51dc3369826d4b8aed6250fceb62136e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f20a18c38fe989d6295276c36fcb5c0b",
+    "url": "/eurovision-scorer/index.html"
+  },
+  {
+    "revision": "b25be42e1cf9176ff493",
+    "url": "/eurovision-scorer/static/css/main.05c6b03d.chunk.css"
+  },
+  {
+    "revision": "fd00326e54411559087a",
+    "url": "/eurovision-scorer/static/js/2.a66f2702.chunk.js"
+  },
+  {
+    "revision": "b25be42e1cf9176ff493",
+    "url": "/eurovision-scorer/static/js/main.920e50ce.chunk.js"
+  },
+  {
+    "revision": "254c43a7eb9073567bd9",
+    "url": "/eurovision-scorer/static/js/runtime~main.fb48de96.js"
+  }
+]);
