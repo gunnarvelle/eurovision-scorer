b0d0dcd422539ffea0b90b069142540c9f2939f9
Eirik Vageskar <eirikvageskar@gmail.com>
2019-05-18 15:14:18 +0200

    Add simple routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import "./App.css";
 import Voting from "./Voting";
 import ScoreBoard from "./ScoreBoard";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <ScoreBoard />
-      <Voting />
+      <Router>
+        <Route path="/" exact component={Voting} />
+        <Route path="/aoeu" exact component={ScoreBoard} />
+      </Router>
     </div>
   );
 };
